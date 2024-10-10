74463325141a6c38a359945c3799b18725e20f88
Maxim Novikov (xMlex) <the.mlex@gmail.com>
2024-10-10 16:01:37 +0300

    tgz build

--- build.sh
+++ build.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
+set -e
+
 mkdir -p build
 
 echo "build linux amd64"
 CGO_ENABLED=0 GOOS="linux" GOARCH="amd64" go build -o build/dns-ipset-linux-amd64 .
+cd build
+tar -cvzf dns-ipset-linux-amd64.tgz dns-ipset-linux-amd64
+cd ../
+
 echo "build linux 386"
 CGO_ENABLED=0 GOOS="linux" GOARCH="386" go build -o build/dns-ipset-linux-386 .
+cd build
+tar -cvzf dns-ipset-linux-386.tgz dns-ipset-linux-386
+cd ../
